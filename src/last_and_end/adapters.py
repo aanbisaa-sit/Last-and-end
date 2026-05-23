e9c4f336f07128f74eaf7c1c9a6628c402038e48
aanbisaa-sit <salfiankendari@gmail.com>
2026-05-23 11:48:33 +0200

    feat(adapters): define notification adapter protocol

---
+++ src/last_and_end/adapters.py
@@ -0,0 +1,8 @@
+from typing import Protocol
+
+from .notifications import NotificationMessage
+
+
+class NotificationAdapter(Protocol):
+def send(self, message: NotificationMessage) -> None:
+    ...
